b8bc9c4febdbfad6eb2cf2cc1d09f5936c920d93
ZiRui <zhang.zr35@gmail.com>
2025-09-18 09:55:03 +0800

    Add

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,17 +6,53 @@
 /*   By: zirui <zirui@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 23:55:49 by zirui             #+#    #+#             */
-/*   Updated: 2025/09/11 23:56:56 by zirui            ###   ########.fr       */
+/*   Updated: 2025/09/18 09:54:07 by zirui            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strcapitalize(char *str)
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
-		if(str[i] >= )
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
 	}
+	return (str);
 }
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+
+	ft_strlowcase(str);
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	i = 1;
+	while (str[i])
+	{
+		if (!((str[i - 1] >= 'a' && str[i - 1] <= 'z')
+				|| (str[i - 1] >= 'A' && str[i - 1] <= 'Z')
+				|| str[i - 1] >= '0' && str[i - 1] <= '9'))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+/*int	main(void)
+{
+	char	str[] = "hello wOrLD, hoW Are yoU?";
+
+	printf("Before: %s\n", str);
+	printf("After: %s\n", ft_strcapitalize(str));
+}
+*/
