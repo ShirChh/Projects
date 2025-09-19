e23183c61528b2d105ebb160d5f7b1e954118f8d
Ruyi <zhang.zr35@gmail.com>
2025-09-19 14:05:37 +0800

    added

---
+++ C04/ex01/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zirui <zirui@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/19 14:02:04 by zirui             #+#    #+#             */
+/*   Updated: 2025/09/19 14:05:18 by zirui            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+/* 
+int main(int argc, char const *argv[])
+{
+	char *str = "Hello World!";
+	ft_putstr(str);
+	return 0;
+} */
