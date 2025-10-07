0ea132c5f0921a8682a2a62f6a207a0ec3c35b4d
Ruyi <zhang.zr35@gmail.com>
2025-10-07 16:06:09 +0800

    added

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: zirui <zirui@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 09:27:23 by zirui             #+#    #+#             */
-/*   Updated: 2025/09/22 12:05:33 by zirui            ###   ########.fr       */
+/*   Updated: 2025/10/07 16:05:36 by zirui            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,17 +59,17 @@ void	ft_putnbr_base(int nbr, char *base)
 
 	if (ft_check_valid(base) == 1)
 		return ;
-
 	nb = nbr;
 	if (nbr < 0)
 	{
 		nb *= -1;
-		ft_putchar("-");
+		ft_putchar('-');
 	}
-
 	base_len = ft_strlen(base);
 	i = 0;
-	while (nb < 0)
+	if (nb == 0)
+		box[i++] = base[0];
+	while (nb > 0)
 	{
 		box[i] = base[nb % base_len];
 		nb = nb / base_len;
@@ -77,4 +77,35 @@ void	ft_putnbr_base(int nbr, char *base)
 	}
 	while (--i >= 0)
 		ft_putchar(box[i]);
+}
+
+void ft_put_newline(void)
+{
+    write(1, "\n", 1);
+}
+
+int main(void)
+{
+    // Test 1: Decimal (Base 10)
+    ft_putnbr_base(42, "0123456789"); // Should print 42
+    ft_put_newline();
+
+    // Test 2: Binary (Base 2)
+    ft_putnbr_base(12, "01"); // Should print 1100
+    ft_put_newline();
+
+    // Test 3: Hexadecimal (Base 16)
+    ft_putnbr_base(255, "0123456789ABCDEF"); // Should print FF
+    ft_put_newline();
+
+    // Test 4: Negative number
+    ft_putnbr_base(-42, "0123456789"); // Should print -42
+    ft_put_newline();
+
+    // Test 5: Invalid base (should print nothing)
+    ft_putnbr_base(123, "0012");
+    ft_putnbr_base(123, "+012");
+    ft_put_newline();
+
+    return (0);
 }
